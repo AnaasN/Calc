927476cd5fba05dfd08565cdb18c2544f6560766
AnaasN <nice-anaas@hotmail.co.uk>
2016-10-17 15:33:23 +0100

    Add files via upload
    updated version(17/10/16)

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AnaasN
+            string UserInput;
+            double Sum;
+            double FirstNumber;
+            double SecondNumber;
+            bool ProgramWorking = true;
+            int AsciiConvert = 48;
+            int PositionOfOperator;
+
+            Console.WriteLine("Basic Calculator");
+            while (ProgramWorking)
+            {
+
+                Console.WriteLine("Enter calculation");
+                UserInput = Console.ReadLine();
+                if (UserInput.Contains('+'))
+                {
+                    PositionOfOperator = UserInput.IndexOf('+');
+                    FirstNumber = UserInput[0] - AsciiConvert;
+                    SecondNumber = UserInput[2] - AsciiConvert;                                                                                                              
+                    Sum = FirstNumber + SecondNumber;
+                    Console.WriteLine("{0} = {1}", UserInput, Math.Round(Sum, 3));
+                }
+                else if (UserInput.Contains('-'))
+                {
+                    Sum = UserInput[0] - UserInput[2];
+                    Console.WriteLine("{0} = {1}", UserInput, Math.Round(Sum, 3));
+                }
+                else if (UserInput.Contains('*'))
+                {
+                    FirstNumber = UserInput[0] - AsciiConvert;
+                    SecondNumber = UserInput[2] - AsciiConvert;
+                    Sum = FirstNumber * SecondNumber;
+                    Console.WriteLine("{0} = {1}", UserInput, Math.Round(Sum, 3));
+                }
+                else if (UserInput.Contains('/'))
+                {
+                    FirstNumber = UserInput[0] - AsciiConvert;
+                    SecondNumber = UserInput[2] - AsciiConvert;
+                    Sum = FirstNumber / SecondNumber;
+                    Console.WriteLine("{0} = {1}", UserInput, Math.Round(Sum, 3));
+                }
+                else Console.WriteLine("Correct operator was not entered, Retry with correct operator (e.i. '+', '-', '/' or '*'.");
+
+            }
+            
+        }
+    }
+}
